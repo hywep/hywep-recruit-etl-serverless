bf03f1fd2a8d01ded9ad06cc405f74f66da38b21
boms03 <dvboms@gmail.com>
2025-01-05 14:07:21 +0900

    feat: add data transformation logic
    Feat: Add data transformation logic for crawled data
    
    - Implemented transformData function to map keys, exclude specific fields, and clean values
    - Added various data processing functions for transforming specific fields
    
    Related to: #1

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,4 +1,7 @@
 import { SQSHandler } from "aws-lambda";
+import * as stream from "stream";
+import {colleges, EXCLUDE_KEYS, INVALID_VALUES, KEY_MAPPING, relatedColleges, special} from "./constants";
+import * as levenshtein from "fast-levenshtein";
 import {saveToElasticsearch} from "./elasticsearch";
 import {saveToDynamoDB} from "./dynamo";
 import {getS3File, saveToS3} from "./s3";
@@ -17,7 +20,7 @@ export const handler: SQSHandler = async (event) => {
 
             console.log("Crawled Data:", crawledData);
 
-            const transformedData = crawledData.map('');
+            const transformedData = crawledData.map(transformData);
 
             const processedKey = key.replace("raw-data", "processed-data");
             await saveToS3(bucketName, processedKey, transformedData);
@@ -30,3 +33,53 @@ export const handler: SQSHandler = async (event) => {
         throw error;
     }
 };
+
+/**
+ * Transforms the input data by mapping keys, excluding specific fields,
+ * and normalizing or cleaning values based on predefined rules.
+ *
+ * @param {Record<string, any>} data - The input data to be transformed.
+ * @returns {Record<string, any>} - The transformed data after applying necessary changes.
+ */
+export function transformData(data: Record<string, any>): Record<string, any> {
+    const transformedData: Record<string, any> = {};
+
+    for (const [key, value] of Object.entries(data)) {
+        const newKey = KEY_MAPPING[key] || key;
+
+        if (EXCLUDE_KEYS.includes(newKey) || INVALID_VALUES.includes(value)) {
+            continue;
+        }
+
+        switch (newKey) {
+            case "deadlineTime":
+                break;
+            case "majors":
+                break;
+            case "internshipPeriod":
+                break;
+            case "organizationSupportAmount":
+                break;
+            case "status":
+                break;
+            case "qualifications":
+                break;
+            case "interviewInfo":
+                break;
+            case "internshipDetails":
+                break;
+            case "workingHours":
+                continue;
+            case "workingDays":
+                break;
+            case "progress":
+                break;
+            case "selectionInfo":
+                break;
+            default:
+                break;
+        }
+    }
+
+    return transformedData;
+}
